dac0295c97d8a6c3d5ba6d1000d6550717f53f84
Fahad Hussain <fahd__hussain@hotmail.com>
2022-11-07 20:23:28 +0500

    ci: added commit linting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,12 @@
-// Build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
-// CI: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
-// Docs: Documentation only changes
-// Feature: A new feature
-// Fix: A bug fix
-// Perf: A code change that improves performance
-// Refactor: A code change that neither fixes a bug nor adds a feature
-// Style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-// Test: Adding missing tests or correcting existing tests
+// build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+// ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
+// docs: Documentation only changes
+// feat: A new feature
+// fix: A bug fix
+// perf: A code change that improves performance
+// refactor: A code change that neither fixes a bug nor adds a feature
+// style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
+// test: Adding missing tests or correcting existing tests
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
@@ -30,20 +30,20 @@ module.exports = {
       2,
       'always',
       [
-        'Build',
-        'Chore',
-        'CI',
-        'Docs',
-        'Feature',
-        'Fix',
-        'Perf',
-        'Refactor',
-        'Revert',
-        'Style',
-        'Test',
-        'Translation',
-        'Security',
-        'Changeset',
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'translation',
+        'security',
+        'changeset',
       ],
     ],
   },
